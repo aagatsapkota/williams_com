58f29545ec720318136a216625fb3ed3f4ebc5c2
Aagat Sapkota <ars9@williams.edu>
2020-09-06 11:02:06 -0400

    first commit

---
+++ src/pages/contact/index.js
@@ -0,0 +1,122 @@
+import React from 'react'
+import { navigate } from 'gatsby-link'
+import Layout from '../../components/Layout'
+
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isValidated: false }
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    // put the api info here
+    var formdata = new FormData();
+    formdata.append("Name", e.target.name.value)
+    formdata.append("Email", e.target.email.value)
+    formdata.append("Message", e.target.message.value)
+
+    var requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+    };
+
+    fetch("https://sheet.best/api/sheets/e95a2230-92af-4d6c-8b89-819629915c3d", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .then(() => navigate(form.getAttribute('action')))
+      .catch(error => console.log('error', error))
+  }
+
+  render() {
+    return (
+      <Layout>
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <h1>Contact</h1>
+              <form
+                name="contact"
+                method="post"
+                action="/contact/thanks/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={this.handleSubmit}
+              >
+                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                <input type="hidden" name="form-name" value="contact" />
+                <div hidden>
+                  <label>
+                    Don’t fill this out:{' '}
+                    <input name="bot-field" onChange={this.handleChange} />
+                  </label>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={'name'}>
+                    Science Journal Name
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type={'text'}
+                      name={'name'}
+                      onChange={this.handleChange}
+                      id={'name'}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={'email'}>
+                    Email
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type={'email'}
+                      name={'email'}
+                      onChange={this.handleChange}
+                      id={'email'}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={'message'}>
+                    Message
+                  </label>
+                  <div className="control">
+                    <textarea
+                      className="textarea"
+                      name={'message'}
+                      onChange={this.handleChange}
+                      id={'message'}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <button className="button is-link" type="submit">
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )
+  }
+}
---
+++ src/pages/contact/thanks.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+export default () => (
+  <Layout>
+    <section className="section">
+      <div className="container">
+        <div className="content">
+          <h1>Thank you for your submission!</h1>
+          <p>We will be revewing your Journal shortly</p>
+        </div>
+      </div>
+    </section>
+  </Layout>
+)
